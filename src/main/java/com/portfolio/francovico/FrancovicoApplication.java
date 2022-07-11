3c4c5f127a2c08b674bd5da86968a461c560bc56
francovico <francoivico@gmail.com>
2022-07-11 20:44:28 -0300

    Primer commit

---
+++ src/main/java/com/portfolio/francovico/FrancovicoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.francovico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrancovicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrancovicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/francovico/FrancovicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.francovico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancovicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
