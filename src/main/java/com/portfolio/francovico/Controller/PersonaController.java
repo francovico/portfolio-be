36d9f6a8ff3b88d355cdaf81186b00f9cfd616bc
francovico <francoivico@gmail.com>
2022-07-12 20:53:14 -0300

    Cors

--- src/main/java/com/portfolio/francovico/Controller/PersonaController.java
+++ src/main/java/com/portfolio/francovico/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.francovico.Entity.Persona;
 import com.portfolio.francovico.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
+
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
