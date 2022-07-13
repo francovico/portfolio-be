922fcc8ebb911097d54c970ef4fa98b663da14c2
francovico <francoivico@gmail.com>
2022-07-12 22:45:26 -0300

    Ultimo

--- src/main/java/com/portfolio/francovico/Controller/PersonaController.java
+++ src/main/java/com/portfolio/francovico/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
@@ -23,6 +23,11 @@ public class PersonaController {
     @GetMapping("personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
+    }
+    
+    @GetMapping("personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
     
     @PostMapping("personas/crear")
