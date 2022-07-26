28ba879d3b083a9e9532273bdef308a72ad191a6
francovico <francoivico@gmail.com>
2022-07-26 14:09:16 -0300

    Cors y Cors

--- src/main/java/com/portfolio/francovico/FrancovicoApplication.java
+++ src/main/java/com/portfolio/francovico/FrancovicoApplication.java
@@ -2,12 +2,25 @@ package com.portfolio.francovico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FrancovicoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrancovicoApplication.class, args);
+	}
+        
+        @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
