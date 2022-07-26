47cbf6013d0d913b9c76012c4347f7b2768ac9a9
francovico <francoivico@gmail.com>
2022-07-26 03:27:31 -0300

    Cors

--- src/main/java/com/portfolio/francovico/Security/MainSecurity.java
+++ src/main/java/com/portfolio/francovico/Security/MainSecurity.java
@@ -10,18 +10,18 @@ import com.portfolio.francovico.Security.Service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  *
@@ -62,6 +62,13 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
         http.addFilterBefore(JWTTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         
     }
+    
+    
+    // Mas pruebas
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    web.ignoring()
+        .antMatchers(HttpMethod.OPTIONS, "/**");}
     
    // @Bean
    // public WebMvcConfigurer corsConfigurer() {
