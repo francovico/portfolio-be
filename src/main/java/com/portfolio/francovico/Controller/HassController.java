1116b0779adc6ee881a74e62e6072dc08ba623f1
francovico <francoivico@gmail.com>
2022-07-26 19:11:26 -0300

    @EnableWebSecurity

--- src/main/java/com/portfolio/francovico/Controller/HassController.java
+++ src/main/java/com/portfolio/francovico/Controller/HassController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("hass")
 @CrossOrigin(origins ="https://portfolio-fe-francovico.herokuapp.com")
+@EnableWebSecurity
 public class HassController {
     @Autowired
     HassService HassService;
--- src/main/java/com/portfolio/francovico/Security/Service/AboutService.java
+++ src/main/java/com/portfolio/francovico/Security/Service/AboutService.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
 import org.springframework.stereotype.Service;
 
 /**
