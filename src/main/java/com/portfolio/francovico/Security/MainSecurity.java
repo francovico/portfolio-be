856230bedb1a315c76eff939b75520bcb7fade2a
francovico <francoivico@gmail.com>
2022-07-26 03:45:54 -0300

    Cors

--- src/main/java/com/portfolio/francovico/Security/MainSecurity.java
+++ src/main/java/com/portfolio/francovico/Security/MainSecurity.java
@@ -10,21 +10,16 @@ import com.portfolio.francovico.Security.Service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  *
@@ -66,32 +61,6 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
         
     }
     
-    
-    // Mas pruebas
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-    web.ignoring()
-        .antMatchers(HttpMethod.OPTIONS, "/**");}
-    
-    
-    // Mas pruebas
-    @Configuration
-    public class CorsConfiguration
-{
-    @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
-    }
-}
-    // Mas pruebas
-    
-
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
