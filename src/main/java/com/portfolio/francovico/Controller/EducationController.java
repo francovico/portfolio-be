42b2421896d1ff6279858c83af963cc6d98288f7
francovico <francoivico@gmail.com>
2022-07-26 01:10:59 -0300

    CORS

--- src/main/java/com/portfolio/francovico/Controller/EducationController.java
+++ src/main/java/com/portfolio/francovico/Controller/EducationController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("education")
-@CrossOrigin(origins ="http://localhost:4200")
+@CrossOrigin(origins ="https://portfolio-fe-francovico.herokuapp.com")
 
 public class EducationController {
     @Autowired
--- src/main/java/com/portfolio/francovico/Controller/ExperienciaController.java
+++ src/main/java/com/portfolio/francovico/Controller/ExperienciaController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("experiencia")
-@CrossOrigin(origins ="http://localhost:4200")
+@CrossOrigin(origins ="https://portfolio-fe-francovico.herokuapp.com")
 
 public class ExperienciaController {
     @Autowired
